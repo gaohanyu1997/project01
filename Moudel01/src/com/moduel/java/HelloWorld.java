3317f910516989487dfbc781c0ee69b5339ebcbf
gaohanyu1997 <88262806+gaohanyu1997@users.noreply.github.com>
2021-09-26 23:40:34 +0800

    Update HelloWorld.java

--- Moudel01/src/com/moduel/java/HelloWorld.java
+++ Moudel01/src/com/moduel/java/HelloWorld.java
@@ -3,6 +3,5 @@ package com.moduel.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World123!");
     }
 }
